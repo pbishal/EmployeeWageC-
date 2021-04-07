c0059e2f8f8ebd3526b9b901d6b0f321f7f86302
pbishal <pbishal60@gmail.com>
2021-04-07 22:49:44 +0530

    Employee part time full time added

--- Program.cs
+++ Program.cs
@@ -6,31 +6,38 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-         //local variables
-         int empWage = 0;
-         int empHrs = 0;
-         int empRatePerHr = 20;
-         //constants
-         int PRESENT = 1;
-         int EMP_RATEPER_HR = 20;
+            local variables
+            int empWage = 0;
+            int empHrs = 0;
+            int empRatePerHr = 20;
+            //constants
+            //int PRESENT = 1;
+            int EMP_RATEPER_HR = 20;
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
 
-         //inbuilt class
-         Random random = new Random();
-         int employeeCheck = random.Next(0, 2);
+            //inbuilt class
+            Random random = new Random();
+            int employeeCheck = random.Next(0, 3);
 
-         Console.WriteLine("Random value" + employeeCheck);
-         if (employeeCheck == PRESENT)
-         {
-            empHrs = 8;
-         }
-         else
-         {
-            empHrs = 0;
-         }
+            Console.WriteLine("Random value" + employeeCheck);
+            if (employeeCheck == FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (employeeCheck == PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
 
-         empWage = EMP_RATEPER_HR * empHrs;
-         Console.WriteLine("Employee wage per day" + empWage);
-         Console.ReadLine();
+            empWage = EMP_RATEPER_HR * empHrs;
+            Console.WriteLine("Employee wage per day" + empWage);
+            Console.ReadLine();
+
         }
     }
 }
