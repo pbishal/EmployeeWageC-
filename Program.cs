5b65c4de46e578729cff8a6f91d2f1199e493450
pbishal <pbishal60@gmail.com>
2021-04-07 22:25:42 +0530

    UC1 added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage problem");
+            int present = 1;
+            Random randomValue = new Random();
+            int employeeCheck = randomValue.Next(0, 2);
+
+            Console.WriteLine("Random value" + employeeCheck);
+
+            if (employeeCheck == present)
+                 Console.WriteLine("Employee is present");
+            else
+                 Console.WriteLine("Employee is absent");
+
+            Console.ReadLine();
+
         }
     }
 }
